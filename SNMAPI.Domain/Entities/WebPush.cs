170aa297165cfb95c6620e8e5d51b30d41525b9d
igorparente182 <figorpdf@gmail.com>
2022-10-17 17:27:12 -0300

    entidade webpush

--- SNMAPI.Domain/Entities/WebPush.cs
+++ SNMAPI.Domain/Entities/WebPush.cs
@@ -12,6 +12,26 @@ namespace SNMAPI.Domain.Entities
 
         public APP app { get; set; }
 
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public string url_icon { get; set; }
+
+        public string message_text { get; set; }
+
+        public string allow_button_text { get; set; }
+
+        public string deny_button_text { get; set; }
+
+        public string message_title { get; set; }
+
+        public string message_text_welcome { get; set; }
+
+        public string enable_url_redirect { get; set; }
+
+        public string url_redirect { get; set; }
+
 
     }
 }
